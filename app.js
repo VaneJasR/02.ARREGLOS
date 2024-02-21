9fb0978bd5ee5d25480dd7d9a449b1b35ea02660
VaneJasR <vjrr2904@gmail.com>
2024-02-20 20:13:11 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+//Ejercicio 1 
+let vacio = Array(null)
+for (let i=1; i<=10; i++){
+    console.log(i)
+}
+
+//Ejercicio 2 
+let str = prompt('ingresa tus palabras separadas por comas ');
+let arr = str.split(',');
+console.log(arr)
+
+//Ejercicio 3 
+let num = [10,40,30,20,15,5]
+console.log(num.sort((a,b)=>a-b))
+
